82b49139c915be1c8424b8effeb7b4802758ff32
Eri_Tsutsui <tsutsui@balance.bz>
2021-10-29 19:04:49 +0900

    init

---
+++ 17_update/carouselInfinitLoop/src/js/src/View/UI/Pages/Top/slider/Order.js
@@ -0,0 +1,60 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+
+export default class Controller extends Base {
+  constructor(len) {
+    super();
+
+    this.len = len;
+    this.current = 0;
+    this.next = 1;
+    this.prev = -1;
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {}
+
+  right() {
+    this.current++;
+    if (this.current > this.len - 1) this.current = this.len - 1;
+    this.next = this.current + 1;
+    this.prev = this.current - 1;
+  }
+
+  left() {
+    this.current--;
+    if (this.current < 0) this.current = 0;
+    this.next = this.current + 1;
+    this.prev = this.current - 1;
+  }
+
+  move(index) {
+    this.current = index;
+    if (this.current > this.len - 1) this.current = this.len - 1;
+    if (this.current < 0) this.current = 0;
+    this.next = this.current + 1;
+    this.prev = this.current - 1;
+  }
+
+  reset() {
+    this.current = 0;
+    this.next = 1;
+    this.prev = -1;
+  }
+
+  onResize() {
+    this.reset();
+  }
+
+  setEvents() {
+    super.setEvents();
+  }
+}
