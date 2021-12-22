b30109b33b86335a9cd804b32458d24ac8e47cec
Eri_Tsutsui <tsutsui@balance.bz>
2021-12-22 17:07:47 +0900

    add: gorogorobtn

--- gorogoro/src/js/src/View/UI/Common/GorogoroBtn/Controller.js
+++ gorogoro/src/js/src/View/UI/Common/GorogoroBtn/Controller.js
@@ -14,6 +14,7 @@ export default class Controller extends Base {
 
     this.isSEv = true;
     this.isUEv = true;
+    this.isREv = true;
 
     this.$btn = $(".js-gorogorobtn");
 
@@ -22,13 +23,16 @@ export default class Controller extends Base {
   }
 
   setup() {
-    this.posX = 0;
-    this.rotaton = 0;
+    this.setParameter();
+  }
+
+  setParameter() {
     this.maxSt = document.body.clientHeight - window.innerHeight; // 最大スクロール量
     this.maxDist = window.innerWidth - this.$btn.width(); // 最大移動距離
     this.maxRotation = 360 * 4; // 最大回転数
 
-    // console.log(this.maxSt, this.maxDist, this.maxRotation);
+    this.posX = 0;
+    this.rotaton = 0;
   }
 
   timeline() {}
@@ -43,31 +47,27 @@ export default class Controller extends Base {
     // });
   }
 
-  onResize() {}
+  onResize() {
+    this.setParameter();
+    this.onScroll();
+  }
 
   move(st) {
     // スクロール量をxとrotationに変換
     // this.st : 0 〜 this.maxSt
     // x : 0 〜 maxDist
     // rotation : 0 〜 360 * 4回転
-
     this.posX = m.map(st, 0, this.maxDist, 0, this.maxSt);
     this.rotation = m.map(st, 0, this.maxRotation, 0, this.maxSt);
-
-    // this.$btn.get(0).style.transform = `translateX(${this.posX})px`;
 
     gsap.set(this.$btn, {
       x: this.posX,
       rotation: this.rotation,
     });
-
-    console.log(this.posX);
-    console.log(this.rotation);
   }
 
   onScroll() {
     this.st = window.scrollY;
-
     this.move(this.st);
   }
 
