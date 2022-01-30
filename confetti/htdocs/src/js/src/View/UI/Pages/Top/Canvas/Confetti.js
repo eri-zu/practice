503a7325ce18b72756df2074485fc761c666311a
Eri_Tsutsui <tsutsui@balance.bz>
2022-01-30 16:59:26 +0900

    add: confetti

--- confetti/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Confetti.js
+++ confetti/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Confetti.js
@@ -11,13 +11,12 @@ import gsap from "gsap";
 import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
 import { Conf } from "@/Conf";
 
-export default class Sakura extends Base {
-  constructor(canvas, ctx, img) {
+export default class Confetti extends Base {
+  constructor(canvas, ctx) {
     super();
 
     this.canvas = canvas;
     this.ctx = ctx;
-    this.img = img;
 
     this.setup();
     this.setEvents();
@@ -25,10 +24,13 @@ export default class Sakura extends Base {
 
   setup() {
     // 大きさランダム係数
-    this.randomScale = m.randomInt(0.5, 2);
+    this.randomScale = {
+      w: m.randomInt(8, 10),
+      h: m.randomInt(8, 15),
+    };
 
-    // 位置y（大きいほど早く落ちる）
-    this.vy = this.randomScale / 0.5;
+    // 位置y
+    this.vy = m.randomInt(5, 10);
 
     // 位置x
     this.radiusX = m.randomInt(-5, 5);
@@ -37,11 +39,23 @@ export default class Sakura extends Base {
 
     // 回転
     this.rotation = Math.random() * 360;
-    this.rotationV = m.randomInt(1, 2);
+    this.rotationV = 3;
 
     // 高さ
     this.degreeH = Math.random() * 360;
-    this.degreeHV = 1;
+    this.degreeHV = 3;
+
+    this.colors = [
+      ["#df0049", "#660671"],
+      ["#00e857", "#005291"],
+      ["#2bebbc", "#05798a"],
+      ["#ffd200", "#b06c00"],
+    ];
+
+    this.frontColor =
+      this.colors[Math.round(Math.random() * (this.colors.length - 1))][0];
+    this.backColor =
+      this.colors[Math.round(Math.random() * (this.colors.length - 1))][1];
 
     this.init();
   }
@@ -50,8 +64,8 @@ export default class Sakura extends Base {
     this.per = Math.min(1, window.innerWidth / 1280);
 
     this.size = {
-      w: 15 * this.randomScale * gb.conf.devicePixelRatio * this.per,
-      h: 18.5 * this.randomScale * gb.conf.devicePixelRatio * this.per,
+      w: this.randomScale.w * gb.conf.devicePixelRatio * this.per,
+      h: this.randomScale.h * gb.conf.devicePixelRatio * this.per,
     };
 
     this.position = {
@@ -61,13 +75,10 @@ export default class Sakura extends Base {
   }
 
   display() {
-    this.ctx.drawImage(
-      this.img,
-      this.position.x,
-      this.position.y,
-      this.size.w,
-      this.size.h
-    );
+    this.ctx.fillStyle = this.frontColor;
+    this.ctx.beginPath();
+    this.ctx.rect(this.position.x, this.position.y, this.size.w, this.size.h);
+    this.ctx.fill();
   }
 
   draw() {
@@ -76,31 +87,26 @@ export default class Sakura extends Base {
     this.position.x += this.vx;
     this.degreeX += this.degreeXV;
 
-    // 落下
+    // // 落下
     this.vy *= 0.9999; // 空気抵抗
     this.position.y += this.vy;
 
-    // 高さ（※高さ変えてx軸に沿って回転してる風に見せる）
+    // // 高さ（※高さ変えてx軸に沿って回転してる風に見せる）
     this.changedH = Math.cos(m.radian(this.degreeH)) * this.size.h;
     this.degreeH += this.degreeHV;
 
-    // 回転
+    // // 回転
     this.rotation += this.rotationV;
 
     // 描画 method1
     this.ctx.translate(this.position.x, this.position.y);
     this.ctx.rotate(m.radian(this.rotation));
-    this.ctx.drawImage(this.img, 0, 0, this.size.w, this.changedH);
+    this.ctx.fillStyle = this.changedH > 0 ? this.frontColor : this.backColor; // ひっくり返ったら色変える
+    this.ctx.beginPath();
+    this.ctx.rect(0, 0, this.size.w, this.changedH);
+    this.ctx.fill();
     this.ctx.rotate(-m.radian(this.rotation));
     this.ctx.translate(-this.position.x, -this.position.y);
-
-    // 描画 method2
-    // this.ctx.save();
-    // this.rotation += this.rotationV;
-    // this.ctx.translate(this.position.x, this.position.y);
-    // this.ctx.rotate(m.radian(this.rotation));
-    // this.ctx.drawImage(this.img, 0, 0, this.size.w, this.size.h);
-    // this.ctx.restore();
 
     // reset
     if (this.position.y > this.canvas.height + this.size.h) {
@@ -110,7 +116,7 @@ export default class Sakura extends Base {
 
   reset() {
     this.position.y = 0 - this.size.h; // 画面外に出たら上に戻す
-    this.vy = this.randomScale / 0.5; // 空気抵抗
+    this.vy = m.randomInt(5, 10);
   }
 
   onResize() {
--- confetti/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Controller.js
+++ confetti/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Controller.js
@@ -6,7 +6,7 @@
 
 import Base from "@BALANCeLibs/Base.js";
 import * as m from "@BALANCeLibs/Util/Math.js";
-import Sakura from "./Sakura";
+import Confetti from "./Confetti";
 import gsap from "gsap";
 import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
 import { Conf } from "@/Conf";
@@ -27,9 +27,6 @@ export default class Controller extends Base {
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = window.innerWidth * gb.conf.devicePixelRatio;
     this.canvas.height = window.innerHeight * gb.conf.devicePixelRatio;
-
-    this.img = new Image();
-    this.img.src = "./assets/resource/img/sakura.png";
   }
 
   ready() {
@@ -37,7 +34,7 @@ export default class Controller extends Base {
     this.array = [];
 
     for (let i = 0; i < len; i++) {
-      this.array.push(new Sakura(this.canvas, this.ctx, this.img));
+      this.array.push(new Confetti(this.canvas, this.ctx));
     }
   }
 
@@ -75,9 +72,7 @@ export default class Controller extends Base {
   setEvents() {
     super.setEvents();
 
-    this.img.addEventListener("load", (e) => {
-      this.ready();
-      this.display();
-    });
+    this.ready();
+    this.display();
   }
 }
