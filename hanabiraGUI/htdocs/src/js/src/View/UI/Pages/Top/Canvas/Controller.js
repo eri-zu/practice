1f83038d10453425095af562896757b138b1e2fe
Eri_Tsutsui <tsutsui@balance.bz>
2022-02-02 15:05:05 +0900

    一旦

---
+++ hanabiraGUI/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Controller.js
@@ -0,0 +1,172 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+import Sakura from "./Sakura";
+import * as dat from "lil-gui";
+
+export default class Controller extends Base {
+  constructor() {
+    super();
+
+    this.isREv = true;
+    this.isUEv = true;
+
+    this.gui = new dat.GUI();
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    this.canvas = document.querySelector("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = window.innerWidth * gb.conf.devicePixelRatio;
+    this.canvas.height = window.innerHeight * gb.conf.devicePixelRatio;
+
+    this.img = new Image();
+    this.img.src = "./assets/resource/img/sakura.png";
+
+    gb.guiParameter = {
+      number: 100,
+      scaleMin: 0.5,
+      scaleMax: 2,
+      vy: 1,
+      degreeHV: 1,
+      rotationV: 1.5,
+      radiusXMin: -5,
+      radiusXMax: 5,
+    };
+
+    this.gui
+      .add(gb.guiParameter, "number")
+      .min(0)
+      .max(500)
+      .step(5)
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "scaleMin")
+      .min(0)
+      .max(10)
+      .step(0.1)
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "scaleMax")
+      .min(0)
+      .max(10)
+      .step(0.1)
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "vy")
+      .min(1)
+      .max(10)
+      .step(0.1)
+      .name("speedY")
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "degreeHV")
+      .min(0)
+      .max(10)
+      .step(0.1)
+      .name("rotation3D")
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "rotationV")
+      .min(0)
+      .max(30)
+      .step(0.1)
+      .name("rotation2D")
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "radiusXMin")
+      .min(-30)
+      .max(30)
+      .step(0.1)
+      .name("rangeXMin")
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+    this.gui
+      .add(gb.guiParameter, "radiusXMax")
+      .min(-30)
+      .max(30)
+      .step(0.1)
+      .name("rangeXMax")
+      .onFinishChange(() => {
+        this.ready();
+        this.display();
+      });
+  }
+
+  ready() {
+    const len = gb.guiParameter.number;
+    this.array = [];
+
+    for (let i = 0; i < len; i++) {
+      this.array.push(new Sakura(this.canvas, this.ctx, this.img));
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.array.length; i++) {
+      this.array[i].display();
+    }
+  }
+
+  update() {
+    // return;
+    this.ctx.clearRect(
+      0,
+      0,
+      window.innerWidth * gb.conf.devicePixelRatio,
+      window.innerHeight * gb.conf.devicePixelRatio
+    );
+
+    if (this.array) {
+      for (let i = 0; i < this.array.length; i++) {
+        this.array[i].draw();
+      }
+    }
+  }
+
+  onResize() {
+    this.canvas.width = window.innerWidth * gb.conf.devicePixelRatio;
+    this.canvas.height = window.innerHeight * gb.conf.devicePixelRatio;
+
+    for (let i = 0; i < this.array.length; i++) {
+      this.array[i].onResize();
+    }
+  }
+
+  setEvents() {
+    super.setEvents();
+
+    this.img.addEventListener("load", (e) => {
+      this.ready();
+      this.display();
+    });
+  }
+}
---
+++ hanabiraGUI/htdocs/src/js/src/View/UI/Pages/Top/Canvas/Sakura.js
@@ -0,0 +1,168 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+
+import gsap from "gsap";
+import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
+import { Conf } from "@/Conf";
+
+export default class Sakura extends Base {
+  constructor(canvas, ctx, img) {
+    super();
+
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.img = img;
+
+    this.ctx.globalCompositeOperation = "multiply";
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    // 大きさランダム係数
+    this.randomScale = m.randomInt(
+      gb.guiParameter.scaleMin,
+      gb.guiParameter.scaleMax
+    );
+
+    // 位置y（大きいほど早く落ちる）
+    this.vy =
+      (this.randomScale / gb.guiParameter.scaleMin) * gb.guiParameter.vy;
+
+    // 位置x
+    this.radiusX = m.randomInt(
+      gb.guiParameter.radiusXMin,
+      gb.guiParameter.radiusXMax
+    );
+    this.degreeX = Math.random() * 360;
+    this.degreeXV = m.randomInt(0.1, 1);
+
+    // 回転
+    this.rotation = Math.random() * 360;
+    // this.rotationV = m.randomInt(1, 2);
+    this.rotationV = gb.guiParameter.rotationV;
+
+    // 高さ
+    this.degreeH = Math.random() * 360;
+    this.degreeHV = gb.guiParameter.degreeHV;
+
+    this.init();
+  }
+
+  init() {
+    // サイズ
+    this.per = Math.min(1, window.innerWidth / 1280);
+    this.size = {
+      w: 15 * this.randomScale * gb.conf.devicePixelRatio * this.per,
+      h: 18.5 * this.randomScale * gb.conf.devicePixelRatio * this.per,
+    };
+
+    // position
+    this.position = {
+      x: Math.random() * this.canvas.width,
+      y: Math.random() * this.canvas.height,
+    };
+  }
+
+  display() {
+    this.ctx.drawImage(
+      this.img,
+      this.position.x,
+      this.position.y,
+      this.size.w,
+      this.size.h
+    );
+  }
+
+  draw() {
+    // 蛇行
+    this.vx = Math.sin(m.radian(this.degreeX)) * this.radiusX;
+    this.position.x += this.vx;
+    this.degreeX += this.degreeXV;
+
+    // 落下
+    this.vy *= 0.9999; // 空気抵抗
+    this.position.y += this.vy;
+
+    // 高さ（※高さ変えてx軸に沿って回転してる風に見せる）
+    this.changedH = Math.cos(m.radian(this.degreeH)) * this.size.h;
+    this.degreeH += this.degreeHV;
+
+    // 回転
+    this.rotation += this.rotationV;
+
+    // 描画 method1
+    this.ctx.translate(this.position.x, this.position.y);
+    this.ctx.rotate(m.radian(this.rotation));
+    this.ctx.drawImage(this.img, 0, 0, this.size.w, this.changedH);
+    this.ctx.rotate(-m.radian(this.rotation));
+    this.ctx.translate(-this.position.x, -this.position.y);
+
+    // 描画 method2
+    // this.ctx.save();
+    // this.rotation += this.rotationV;
+    // this.ctx.translate(this.position.x, this.position.y);
+    // this.ctx.rotate(m.radian(this.rotation));
+    // this.ctx.drawImage(this.img, 0, 0, this.size.w, this.size.h);
+    // this.ctx.restore();
+
+    // reset
+    if (this.finished()) this.reset();
+  }
+
+  finished() {
+    return this.position.y > this.canvas.height + this.size.h;
+  }
+
+  reset() {
+    // this.position = {
+    //   x: Math.random() * this.canvas.width,
+    //   y: 0 - this.size.h, // 画面外に出たら上に戻す
+    // };
+    // this.vy = this.randomScale / 0.5; // 空気抵抗
+
+    // 大きさランダム係数
+    this.randomScale = m.randomInt(
+      gb.guiParameter.scaleMin,
+      gb.guiParameter.scaleMax
+    );
+
+    // 位置y（大きいほど早く落ちる）
+    this.vy =
+      (this.randomScale / gb.guiParameter.scaleMin) * gb.guiParameter.vy;
+
+    // 位置x
+    this.radiusX = m.randomInt(
+      gb.guiParameter.radiusXMin,
+      gb.guiParameter.radiusXMax
+    );
+    this.degreeX = Math.random() * 360;
+    this.degreeXV = m.randomInt(0.1, 1);
+
+    // 回転
+    this.rotation = Math.random() * 360;
+    // this.rotationV = m.randomInt(1, 2);
+    this.rotationV = gb.guiParameter.rotationV;
+
+    // 高さ
+    this.degreeH = Math.random() * 360;
+    this.degreeHV = gb.guiParameter.degreeHV;
+
+    this.position.y = 0 - this.size.h;
+  }
+
+  onResize() {
+    this.init();
+  }
+
+  setEvents() {
+    super.setEvents();
+  }
+}
