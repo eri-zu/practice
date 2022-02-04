b319a3ad21f87a47339c1aa751f8446156d80a9b
Eri_Tsutsui <tsutsui@balance.bz>
2022-02-04 14:28:48 +0900

    add: shader

--- threejsJourney/20-shader/src/js/src/View/UI/Pages/Top/Mv/canvas/Scenes/Scene01/MyModel/Controller.js
+++ threejsJourney/20-shader/src/js/src/View/UI/Pages/Top/Mv/canvas/Scenes/Scene01/MyModel/Controller.js
@@ -8,6 +8,7 @@ import Base from "@BALANCeLibs/Base.js";
 import * as THREE from "three";
 import testVertexShader from "./shaders/test/vertex.glsl";
 import testFragmentShader from "./shaders/test/fragment.glsl";
+import * as dat from "lil-gui";
 
 export default class Plane extends Base {
   constructor(scene, camera, renderer) {
@@ -16,6 +17,11 @@ export default class Plane extends Base {
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
+
+    this.gui = new dat.GUI();
+    this.clock = new THREE.Clock();
+
+    this.isUEv = true;
 
     this.setup();
     this.setEvents();
@@ -31,6 +37,9 @@ export default class Plane extends Base {
 
   loadTexture() {
     this.textureLoader = new THREE.TextureLoader();
+    this.flagTexture = this.textureLoader.load(
+      "./assets/resource/img/flag-french.jpg"
+    );
   }
 
   ready() {
@@ -40,6 +49,24 @@ export default class Plane extends Base {
     this.geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32); // w, h, wseg, hseg
 
     /**
+     * attribute
+     */
+
+    const count = this.geometry.attributes.position.count;
+    const randoms = new Float32Array(count); // lengthがcountのarrayを作成
+    for (let i = 0; i < count; i++) {
+      randoms[i] = Math.random();
+    }
+
+    // geometryにaRandomという名前でnew THREE.BufferAttribute(randoms, 1)というvalue を持つattributeを作る
+    this.geometry.setAttribute(
+      "aRandom", //name
+      new THREE.BufferAttribute(randoms, 1) // randomsという配列をbufferattributeに変換
+    );
+
+    console.log(this.geometry);
+
+    /**
      * material
      * RawShaderMaterialになってもmaterial propertyのうちいくつかは引き継がれる
      * map, alphamap, opacity, colorらへんは無理
@@ -47,13 +74,34 @@ export default class Plane extends Base {
     this.material = new THREE.RawShaderMaterial({
       vertexShader: testVertexShader,
       fragmentShader: testFragmentShader,
+      uniforms: {
+        uFrequeny: { value: new THREE.Vector2(10, 5) }, // objectで渡すこと（typeはもう渡さなくていい）
+        uTime: { value: 0 },
+        uColor: { value: new THREE.Color("orange") },
+        uTexture: { value: this.flagTexture },
+      },
       side: THREE.DoubleSide, // 裏表描画
+      transparent: true,
     });
+
+    this.gui
+      .add(this.material.uniforms.uFrequeny.value, "x")
+      .min(0)
+      .max(20)
+      .step(0.01)
+      .name("frequencyX");
+    this.gui
+      .add(this.material.uniforms.uFrequeny.value, "y")
+      .min(0)
+      .max(20)
+      .step(0.01)
+      .name("frequencyY");
 
     /**
      * mesh
      */
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.scale.y = 2 / 3;
   }
 
   add() {
@@ -61,7 +109,10 @@ export default class Plane extends Base {
   }
 
   update() {
-    this.mesh.rotation.set(gb.up.frame / 100, gb.up.frame / 50, 0);
+    const elapsedTime = this.clock.getElapsedTime();
+
+    // update material
+    this.material.uniforms.uTime.value = elapsedTime;
   }
 
   draw() {}
--- threejsJourney/20-shader/src/js/src/View/UI/Pages/Top/Mv/canvas/Scenes/Scene01/MyModel/shaders/test/vertex.glsl
+++ threejsJourney/20-shader/src/js/src/View/UI/Pages/Top/Mv/canvas/Scenes/Scene01/MyModel/shaders/test/vertex.glsl
@@ -1,17 +1,35 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequeny;
+uniform float uTime;
 
-attribute vec3 position;
+attribute vec3 position; // デフォルトattribute
+attribute vec2 uv; // デフォルトattribute
+attribute float aRandom; // 作ったattribute
+
+varying vec2 vUv; // fragementに送るが、同じ名前は使えないので変えること
+varying float vElevation;
+
 
 void main() {
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-  float a = 1.0;
-  int b = 2;
-  float c = a + float(b);
-  vec2 foo = vec2(0.0);
-  foo.x = 1.0;
-  foo.y = 2.0;
+  float elevation = sin(modelPosition.x * uFrequeny.x + uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequeny.y + uTime) * 0.1;
 
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  // modelPosition.x * 10.0 周期
+  // 0.1 振幅の大きさ
+  // modelPosition.z += sin(modelPosition.x * uFrequeny.x + uTime) * 0.1;
+  // modelPosition.z += sin(modelPosition.y * uFrequeny.y + uTime) * 0.1;
+  modelPosition.z += elevation;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectPosition;
+
+  vUv = uv;
+  vElevation = elevation;
+
 }
