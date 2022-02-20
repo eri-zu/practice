18c6b7a2e740e50dce4db6019f5ee1b60df9009b
Eri_Tsutsui <tsutsui@balance.bz>
2022-02-20 22:34:34 +0900

    fix: textSplit module

--- textSplit/src/js/src/View/UI/Pages/Top/TextSplit/Controller.js
+++ textSplit/src/js/src/View/UI/Pages/Top/TextSplit/Controller.js
@@ -7,52 +7,27 @@
 import Base from "@BALANCeLibs/Base.js";
 import * as m from "@BALANCeLibs/Util/Math.js";
 import gsap from "gsap";
-import { CustomEase } from "@BALANCeLibs/View/gsap/CustomEase/CustomEase.js";
-import { Conf } from "@/Conf";
+import Item from "./Item";
 
-export default class TextSplitController extends Base {
+export default class Controller extends Base {
   constructor() {
     super();
 
-    // this.$item = $(".js-textsplit_txt");
-    this.item = document.querySelector(".js-textsplit_txt");
+    this.items = document.querySelector(".js-textsplit_txt");
+    this.array = [];
 
     this.setup();
     this.setEvents();
   }
 
   setup() {
-    this.split();
+    for (let i = 0; i < this.items.length; i++) {
+      const item = new Item(this.items[i]);
+      this.array.push(item);
+    }
   }
 
-  split() {
-    // html取得
-    const itemHTML = this.item.innerHTML;
-
-    // 文字を<br>で分割
-    // str.split(区切り文字・正規表現, 分割数) : strを区切り文字で分割
-    // 戻り値は配列
-    const separator = /<br>/g;
-    const splitTextArray = itemHTML.split(separator);
-
-    // txtをspanで囲む
-    // replace(検索文字, 置換文字)
-    // // : 検索条件
-    // \S : 空白文字以外
-    // () : グループ化
-    // /g : 該当するもの全て
-    // $1 : ()の戻り値が前から順番に$1, $2,,,
-    const txt = splitTextArray.map((el, i) => {
-      return el.replace(/(\S)/g, "<span>$1</span>");
-    });
-
-    // <br>で連結
-    // join : 配列の全要素を連結した文字列を新たに作成して返す
-    const txtAll = txt.join("<br>");
-
-    // 要素内を書き換える
-    this.item.innerHTML = txtAll;
-  }
+  timeline() {}
 
   update() {}
 
--- textSplit/src/js/src/View/UI/Pages/Top/TextSplit/Item.js
+++ textSplit/src/js/src/View/UI/Pages/Top/TextSplit/Item.js
@@ -1,1 +1,60 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
 
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+
+export default class Item extends Base {
+  constructor(item) {
+    super();
+
+    this.item = item;
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    this.split();
+  }
+
+  split() {
+    // html取得
+    const itemHTML = this.item.innerHTML;
+
+    // 文字を<br>で分割
+    // str.split(区切り文字・正規表現, 分割数) : strを区切り文字で分割
+    // 戻り値は配列
+    const separator = /<br>/g;
+    const splitTextArray = itemHTML.split(separator);
+
+    // txtをspanで囲む
+    // replace(検索文字, 置換文字)
+    // // : 検索条件
+    // \S : 空白文字以外
+    // () : グループ化
+    // /g : 該当するもの全て
+    // $1 : ()の戻り値が前から順番に$1, $2,,,
+    const txt = splitTextArray.map((el, i) => {
+      return el.replace(/(\S)/g, "<span>$1</span>");
+    });
+
+    // <br>で連結
+    // join : 配列の全要素を連結した文字列を新たに作成して返す
+    const txtAll = txt.join("<br>");
+
+    // 要素内を書き換える
+    this.item.innerHTML = txtAll;
+  }
+
+  update() {}
+
+  onResize() {}
+
+  setEvents() {
+    super.setEvents();
+  }
+}
