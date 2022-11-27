7427f26904ebeb3c577434a9c928315b03e83a03
Eri_Tsutsui <tsutsui@balance.bz>
2022-11-27 21:48:15 +0900

    add: template

---
+++ udemy_threejs/01/src/js/src/View/UI/Pages/Top/Canvas/Objs/Controller.js
@@ -0,0 +1,40 @@
+//--------------------------------------------------
+//
+//  Controller
+//
+//--------------------------------------------------
+
+import Base from "@BALANCeLibs/Base.js";
+import * as m from "@BALANCeLibs/Util/Math.js";
+import gsap from "gsap";
+import Box from "./Box/Controller";
+import Plane from "./Plane/Controller";
+
+export default class Controller extends Base {
+  constructor(scene) {
+    super();
+
+    this.scene = scene;
+
+    this.setup();
+    this.setEvents();
+  }
+
+  setup() {
+    // this.box = new Box(this.scene);
+    this.plane = new Plane(this.scene);
+  }
+
+  add() {}
+
+  update() {
+    // this.box.update();
+    this.plane.update();
+  }
+
+  onResize() {}
+
+  setEvents() {
+    super.setEvents();
+  }
+}
