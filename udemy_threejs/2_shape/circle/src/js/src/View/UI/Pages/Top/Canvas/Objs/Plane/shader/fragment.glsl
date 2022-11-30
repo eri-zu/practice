f8e5dfe67b4c52b76f470214dbadd4605aa9aa3c
Eri_Tsutsui <tsutsui@balance.bz>
2022-11-30 16:47:24 +0900

    add:

--- udemy_threejs/2_shape/circle/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/fragment.glsl
+++ udemy_threejs/2_shape/circle/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/fragment.glsl
@@ -1,13 +1,29 @@
 precision mediump float;
-varying vec2 vUv; // fragementに送るが、同じ名前は使えないので変えること
+varying vec2 vUv;
 varying vec3 vPosition;
 
-void main() {
-  // 半径
-  float r = float(length(vPosition.xy));
+bool inCircle(vec3 position, vec2 center, float r) {
+  // position : ピクセルの位置
+  // center : 円の中心座標
+  // r : 半径
 
-  // 半径以下は0.0, 半径以上は0.5 => 1.0から引いて色反転
-  // 黄色にしたいので、vec3(1.0, 1.0, 0.0)積算
-  vec3 color = vec3(1.0, 1.0, 0.0) * float(1.0 - step(0.5, r)); 
+  float dist = length(position.xy - center.xy); // 中心からの距離
+
+  if(dist < r) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void main() {
+  vec3 color = vec3(1.0, 1.0, 1.0);
+
+  if(inCircle(vPosition, vec2(0.5), 0.3)) {
+    color *= vec3(1.0, 1.0, 0.0);
+  } else {
+    color *= vec3(0.0, 0.0, 0.0);
+  }
+
   gl_FragColor = vec4(color, 1.0);
 }
--- udemy_threejs/2_shape/square/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/vertex.glsl
+++ udemy_threejs/2_shape/square/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/vertex.glsl
@@ -4,6 +4,7 @@ uniform mat4 modelMatrix;
 attribute vec3 position; // デフォルトattribute
 attribute vec2 uv; // デフォルトattribute
 varying vec2 vUv; // fragementに送るが、同じ名前は使えないので変えること
+varying vec3 vPosition; // fragementに送るが、同じ名前は使えないので変えること
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
@@ -11,4 +12,5 @@ void main() {
   vec4 projectPosition = projectionMatrix * viewPosition;
   gl_Position = projectPosition;
   vUv = uv;
+  vPosition = position;
 }
