7be4d33593bc0e15e84b0f0c6d1e64ebe31423ab
Eri_Tsutsui <tsutsui@balance.bz>
2022-12-01 15:51:24 +0900

    fix: soften circle

--- udemy_threejs/2_shape/strokeCircle/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/fragment.glsl
+++ udemy_threejs/2_shape/strokeCircle/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/fragment.glsl
@@ -2,14 +2,15 @@ precision mediump float;
 varying vec2 vUv;
 varying vec3 vPosition;
 
-bool inCircle(vec3 position, vec2 center, float r) {
+bool inCircle(vec3 position, vec2 center, float r, float lineWidth) {
   // position : ピクセルの位置
   // center : 円の中心座標
   // r : 半径
+  // lineWidth : stroke太さ
 
   float dist = length(position.xy - center.xy); // 中心からの距離
 
-  if(dist < r) {
+  if(dist < r && dist > r - lineWidth) {
     return true;
   } else {
     return false;
@@ -19,7 +20,7 @@ bool inCircle(vec3 position, vec2 center, float r) {
 void main() {
   vec3 color = vec3(1.0, 1.0, 1.0);
 
-  if(inCircle(vPosition, vec2(0.5), 0.3)) {
+  if(inCircle(vPosition, vec2(0.5), 0.3, 0.05)) {
     color *= vec3(1.0, 1.0, 0.0);
   } else {
     color *= vec3(0.0, 0.0, 0.0);
