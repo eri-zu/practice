a7f6ca3e8043f6fdc4125ba0764d06e0757ad27e
Eri_Tsutsui <tsutsui@balance.bz>
2022-12-01 11:16:44 +0900

    add

---
+++ udemy_threejs/2_shape/strokeCircle/src/js/src/View/UI/Pages/Top/Canvas/Objs/Plane/shader/fragment.glsl
@@ -0,0 +1,29 @@
+precision mediump float;
+varying vec2 vUv;
+varying vec3 vPosition;
+
+bool inCircle(vec3 position, vec2 center, float r) {
+  // position : ピクセルの位置
+  // center : 円の中心座標
+  // r : 半径
+
+  float dist = length(position.xy - center.xy); // 中心からの距離
+
+  if(dist < r) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void main() {
+  vec3 color = vec3(1.0, 1.0, 1.0);
+
+  if(inCircle(vPosition, vec2(0.5), 0.3)) {
+    color *= vec3(1.0, 1.0, 0.0);
+  } else {
+    color *= vec3(0.0, 0.0, 0.0);
+  }
+
+  gl_FragColor = vec4(color, 1.0);
+}
