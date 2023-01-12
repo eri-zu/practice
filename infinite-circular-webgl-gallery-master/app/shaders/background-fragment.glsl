61403a7501e3ad6ff88d4c5da0d1da31dede94d9
Eri_Tsutsui <tsutsui@balance.bz>
2023-01-12 20:00:19 +0900

    一旦

--- infinite-circular-webgl-gallery-master/app/shaders/background-fragment.glsl
+++
@@ -1,9 +0,0 @@
-precision highp float;
-
-uniform float uAlpha;
-uniform vec3 uColor;
-
-void main() {
-  gl_FragColor.rgb = uColor;
-  gl_FragColor.a = 1.0;
-}
--- trigonometry/src/js/BALANCeLibs/View/gsap/index.js
+++
@@ -1,8 +0,0 @@
-import gsap from "gsap";
-//drawSVG
-import { DrawSVGPlugin } from "./DrawSVG/DrawSVGPlugin.js";
-gsap.registerPlugin(DrawSVGPlugin);
-
-//customEase
-import { CustomEase } from "./CustomEase/CustomEase.js";
-gsap.registerPlugin(CustomEase);
